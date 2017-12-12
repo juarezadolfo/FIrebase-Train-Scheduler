ab5cf801e1779ffb4afb54516f0be126ba17b766
juarezadolfo <juarezadolfo@hotmail.com>
2017-12-11 18:50:27 -0800

    first commit - created files, git, etc...

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyClVnromzV5t1SEJFU_zR87uX7FrFeIn_E",
+    authDomain: "fir-train-schedule.firebaseapp.com",
+    databaseURL: "https://fir-train-schedule.firebaseio.com",
+    projectId: "fir-train-schedule",
+    storageBucket: "",
+    messagingSenderId: "139447055002"
+  };
+  firebase.initializeApp(config);
